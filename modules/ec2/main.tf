64432e755f566ddf406f29b103df62a99597cb8a
Biswajits-Hub <biswajit24333@gmail.com>
2025-07-21 19:09:10 +0530

    submit

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,6 +3,21 @@ resource "aws_instance" "cd_target" {
   instance_type = "t2.micro"
   subnet_id = "subnet-08cd3b75f943c88c4"
 
+   user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y ruby wget
+              yum install -y httpd
+              cd /home/ec2-user
+              wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+              chmod +x ./install
+              ./install auto
+              systemctl start codedeploy-agent
+              systemctl enable codedeploy-agent
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+
   tags = {
     Name = var.ec2_tag_name_value
   }
